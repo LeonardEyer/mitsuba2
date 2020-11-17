bdb2f367784e3df9a1acc74e4dd96a3cf8f64b93
leonardeyer <le0nard@me.com>
2020-11-17 16:29:37 +0100

    Add getter method bindings. Make enoki work with custom scalar_spectral8 variant. Implement put method.
    Signed-off-by: leonardeyer <le0nard@me.com>

--- include/mitsuba/render/histogram.h
+++ include/mitsuba/render/histogram.h
@@ -7,10 +7,47 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
+/**
+ * \brief Storage for the energy decay envelope of all recorded spectral
+ * frequencies
+ *
+ * This class contains all the information regarding the decay of energy per
+ * frequency. For each (discrete) time step the recorded energy can be stored.
+ */
 template <typename Float, typename Spectrum>
 class MTS_EXPORT_RENDER Histogram : public Object {
+    MTS_IMPORT_TYPES()
 public:
-    Histogram();
+    /**
+     * Construct a new histogram
+     *
+     * \param channel_count
+     *    how many frequencies should be recorded
+     * \param time_step_count
+     *    defines the amount of time slots
+     */
+    Histogram(int channel_count, int time_step_count);
+
+    /**
+     * Insert spectral values with corresponding time indices
+     */
+    Mask put(const UInt32 &time_step, const Spectrum &value, Mask active = true);
+
+    /// Clear everything to zero.
+    void clear();
+
+    // =============================================================
+    //! @{ \name Accesors
+    // =============================================================
+
+    /// Return the number of channels stored
+    int channel_count() { return m_channel_count; }
+
+    /// Return the count of recordable time steps
+    int time_step_count() { return m_time_step_count; }
+
+    //! @}
+    // =============================================================
 
     std::string to_string() const override;
 
@@ -18,6 +55,11 @@ public:
 protected:
     /// Virtual destructor
     virtual ~Histogram();
+
+protected:
+    int m_channel_count;
+    int m_time_step_count;
+    DynamicBuffer<Spectrum> m_data;
 };
 
 MTS_EXTERN_CLASS_RENDER(Histogram)
--- src/librender/histogram.cpp
+++ src/librender/histogram.cpp
@@ -2,13 +2,36 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
-MTS_VARIANT Histogram<Float, Spectrum>::Histogram() {}
+MTS_VARIANT Histogram<Float, Spectrum>::Histogram(int channel_count,
+                                                  int time_step_count)
+    : m_channel_count(channel_count), m_time_step_count(time_step_count) {
 
-MTS_VARIANT Histogram<Float, Spectrum>::~Histogram() { }
+    // Allocate empty buffer
+    m_data = empty<DynamicBuffer<Spectrum>>(time_step_count);
+}
+
+MTS_VARIANT typename Histogram<Float, Spectrum>::Mask
+Histogram<Float, Spectrum>::put(const UInt32 &time_step, const Spectrum &value,
+                                Mask active) {
+    return active;
+}
+
+MTS_VARIANT void Histogram<Float, Spectrum>::clear() {
+    size_t size = m_time_step_count;
+    if constexpr (!is_cuda_array_v<Float>)
+        Throw("\"clear\" not implemented for gpu variant");
+        //memset(m_data.data(), 0, size * m_channel_count* sizeof(ScalarFloat));
+    else
+        m_data = zero<DynamicBuffer<Spectrum>>(size);
+}
+
+MTS_VARIANT Histogram<Float, Spectrum>::~Histogram() {}
 
 MTS_VARIANT std::string Histogram<Float, Spectrum>::to_string() const {
     std::ostringstream oss;
     oss << "Histogram[" << std::endl
+        << "  channel_count = " << m_channel_count << "," << std::endl
+        << "  time_step_count = " << m_time_step_count << "," << std::endl
         << "]";
     return oss.str();
 }
