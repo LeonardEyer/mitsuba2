10b3b238c3867e2eb0b6639e22047381e96f47f4
leonardeyer <le0nard@me.com>
2020-11-18 11:52:12 +0100

    Change DynamicBuffer to simply use Float. Make clear actually work on cpu. Implement put method.
    Signed-off-by: leonardeyer <le0nard@me.com>

--- include/mitsuba/render/histogram.h
+++ include/mitsuba/render/histogram.h
@@ -10,7 +10,42 @@ NAMESPACE_BEGIN(mitsuba)
 template <typename Float, typename Spectrum>
 class MTS_EXPORT_RENDER Histogram : public Object {
 public:
-    Histogram();
+    /**
+     * Construct a new histogram
+     *
+     * \param channel_count
+     *    how many frequencies should be recorded
+     * \param time_step_count
+     *    defines the amount of time slots
+     */
+    Histogram(int channel_count, int time_step_count);
+
+    /**
+     * Insert spectral values with corresponding time indices
+     */
+    Mask put(const UInt32 &time_step, const Spectrum &value, Mask active = true);
+
+    /// Clear everything to zero.
+    void clear();
+
+    // =============================================================
+    //! @{ \name Accesors
+    // =============================================================
+
+    /// Return the number of channels stored
+    int channel_count() { return m_channel_count; }
+
+    /// Return the count of recordable time steps
+    int time_step_count() { return m_time_step_count; }
+
+    /// Return the underlying spectrum buffer
+    DynamicBuffer<Float> &data() { return m_data; }
+
+    /// Return the underlying spectrum buffer (const version)
+    const DynamicBuffer<Float> &data() const { return m_data; }
+
+    //! @}
+    // =============================================================
 
     std::string to_string() const override;
 
@@ -18,6 +53,11 @@ public:
 protected:
     /// Virtual destructor
     virtual ~Histogram();
+
+protected:
+    int m_channel_count;
+    int m_time_step_count;
+    DynamicBuffer<Float> m_data;
 };
 
 MTS_EXTERN_CLASS_RENDER(Histogram)
--- src/librender/histogram.cpp
+++ src/librender/histogram.cpp
@@ -1,10 +1,38 @@
 #include <mitsuba/render/histogram.h>
 
+
 NAMESPACE_BEGIN(mitsuba)
 
 MTS_VARIANT Histogram<Float, Spectrum>::Histogram() {}
 
-MTS_VARIANT Histogram<Float, Spectrum>::~Histogram() { }
+    // Allocate empty buffer
+    m_data = empty<DynamicBuffer<Float>>(time_step_count * channel_count);
+}
+
+MTS_VARIANT typename Histogram<Float, Spectrum>::Mask
+Histogram<Float, Spectrum>::put(const UInt32 &time_step, const Spectrum &value,
+                                Mask active) {
+
+    size_t max = m_time_step_count;
+    UInt32 offset = (time_step * m_channel_count);
+    Mask enabled = active && all(time_step >= 0u && time_step < max);
+
+    for (int i = 0; i < m_channel_count; ++i) {
+        scatter_add(m_data, value[i], offset + i, enabled);
+    }
+
+    return enabled;
+}
+
+MTS_VARIANT void Histogram<Float, Spectrum>::clear() {
+    size_t size = m_time_step_count * m_channel_count;
+    if constexpr (!is_cuda_array_v<Float>)
+        memset(m_data.data(), 0, size * sizeof(ScalarFloat));
+    else
+        m_data = zero<DynamicBuffer<Float>>(size);
+}
+
+MTS_VARIANT Histogram<Float, Spectrum>::~Histogram() {}
 
 MTS_VARIANT std::string Histogram<Float, Spectrum>::to_string() const {
     std::ostringstream oss;
