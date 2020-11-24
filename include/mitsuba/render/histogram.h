9e5938411dd283c2726ee317a2ba2c91094ec9d8
leonardeyer <le0nard@me.com>
2020-11-24 12:15:52 +0100

    Histogram now works with floats as time steps. These too will be discretized.
    Signed-off-by: leonardeyer <le0nard@me.com>

--- include/mitsuba/render/histogram.h
+++ include/mitsuba/render/histogram.h
@@ -7,21 +7,31 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
+/**
+ * \brief Storage for the energy decay envelope of all recorded spectral
+ * wavelengths
+ *
+ * This class contains all the information regarding the decay of energy per
+ * wavelength. For each (discrete) time step the recorded energy can be stored.
+ */
 template <typename Float, typename Spectrum>
 class MTS_EXPORT_RENDER Histogram : public Object {
 public:
     /**
-     * Construct a new histogram
+     * Construct a new histogram for the logging of wavelengths over time
      *
      * \param bin_count
      *    discretize wavelengths into this many bins
      * \param time_step_count
-     *    defines the amount of time
-     * \param range
+     *    discretize time into this many bins
+     * \param wav_range
      *    the wavelength range to be recorded
+     * \param time_range
+     *    the time range to be recorded
      *
      */
-    Histogram(size_t bin_count, size_t time_step_count, ScalarPoint2f range);
+    Histogram(size_t bin_count, size_t time_step_count, ScalarPoint2f wav_range,
+              ScalarPoint2f time_range);
 
     /**
      * \brief Insert Wavelength samples with corresponding time indices
@@ -33,9 +43,12 @@ public:
      * \param value
      *     Intensity value for this wavelength
      * \param active
-     * \return
+     *     Mask indicating enabled state
+     * \returns
+     *     False if the sample was invalid. E.g not in range or NaN
      */
-    Mask put(const UInt32 &time_step, const Wavelength &wavelengths, const Spectrum &value, Mask active = true);
+    Mask put(const Float &time_step, const Wavelength &wavelength,
+             const Spectrum &value, Mask active = true);
 
     /// Clear everything to zero.
     void clear();
@@ -51,7 +64,10 @@ public:
     size_t time_step_count() { return m_time_step_count; }
 
     // Return the wavelength range this histogram is recording
-    ScalarPoint2f range() { return m_range; }
+    ScalarPoint2f wav_range() { return m_wav_range; }
+
+    // Return the time range this histogram is recording
+    ScalarPoint2f time_range() { return m_time_range; }
 
     /// Return the underlying spectrum buffer
     DynamicBuffer<Float> &data() { return m_data; }
@@ -71,23 +87,24 @@ protected:
 
     /**
      * Compute the bin index of this value. This is needed for the
-     * discretization of the different wavelength bins
+     * discretization of the different wavelength and time bins
      *
      * \param value
-     *    The value of a wavelength
+     *    The value to be discretizied
      *
      */
-    UInt32 bin_index(const Float value) {
-        const float bin_size = m_range.y() - m_range.x();
-        const float discretizer = bin_size / m_bin_count;
+    UInt32 discretize(const Float value, ScalarPoint2f range, ssize_t n_steps) {
+        const float bin_size    = range.y() - range.x();
+        const float discretizer = bin_size / n_steps;
         // Integer division
-        return (value - m_range.x()) / discretizer;
+        return (value - range.x()) / discretizer;
     }
 
 protected:
     size_t m_bin_count;
     size_t m_time_step_count;
-    ScalarPoint2f m_range;
+    ScalarPoint2f m_wav_range;
+    ScalarPoint2f m_time_range;
     DynamicBuffer<Float> m_data;
 };
 
