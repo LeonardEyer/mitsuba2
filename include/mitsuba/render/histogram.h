c50804229d991323ed10ad3767e9c57d7589d55b
leonardeyer <le0nard@me.com>
2020-11-24 11:29:15 +0100

    Redesign of Histogram. It now works by discretization of a set wavelength range.
    Signed-off-by: leonardeyer <le0nard@me.com>

--- include/mitsuba/render/histogram.h
+++ include/mitsuba/render/histogram.h
@@ -21,17 +21,29 @@ public:
     /**
      * Construct a new histogram
      *
-     * \param channel_count
-     *    how many frequencies should be recorded
+     * \param bin_count
+     *    discretize wavelengths into this many bins
      * \param time_step_count
-     *    defines the amount of time slots
+     *    defines the amount of time
+     * \param range
+     *    the wavelength range to be recorded
+     *
      */
-    Histogram(int channel_count, int time_step_count);
+    Histogram(size_t bin_count, size_t time_step_count, ScalarPoint2f range);
 
     /**
-     * Insert spectral values with corresponding time indices
+     * \brief Insert Wavelength samples with corresponding time indices
+     *
+     * \param time_step
+     *     Recorded time of this sample
+     * \param wavelengths
+     *     The sampled wavelength
+     * \param value
+     *     Intensity value for this wavelength
+     * \param active
+     * \return
      */
-    Mask put(const UInt32 &time_step, const Spectrum &value, Mask active = true);
+    Mask put(const UInt32 &time_step, const Wavelength &wavelengths, const Spectrum &value, Mask active = true);
 
     /// Clear everything to zero.
     void clear();
@@ -40,11 +52,14 @@ public:
     //! @{ \name Accesors
     // =============================================================
 
-    /// Return the number of channels stored
-    int channel_count() { return m_channel_count; }
+    /// Return the number of stored bins
+    size_t bin_count() { return m_bin_count; }
 
     /// Return the count of recordable time steps
-    int time_step_count() { return m_time_step_count; }
+    size_t time_step_count() { return m_time_step_count; }
+
+    // Return the wavelength range this histogram is recording
+    ScalarPoint2f range() { return m_range; }
 
     /// Return the underlying spectrum buffer
     DynamicBuffer<Float> &data() { return m_data; }
@@ -62,9 +77,25 @@ protected:
     /// Virtual destructor
     virtual ~Histogram();
 
+    /**
+     * Compute the bin index of this value. This is needed for the
+     * discretization of the different wavelength bins
+     *
+     * \param value
+     *    The value of a wavelength
+     *
+     */
+    UInt32 bin_index(const Float value) {
+        const float bin_size = m_range.y() - m_range.x();
+        const float discretizer = bin_size / m_bin_count;
+        // Integer division
+        return (value - m_range.x()) / discretizer;
+    }
+
 protected:
-    int m_channel_count;
-    int m_time_step_count;
+    size_t m_bin_count;
+    size_t m_time_step_count;
+    ScalarPoint2f m_range;
     DynamicBuffer<Float> m_data;
 };
 
