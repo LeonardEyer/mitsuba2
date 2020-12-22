b1f513f0bfc7bbcc08171b2d2d73edbff006d19a
leonardeyer <le0nard@me.com>
2020-12-22 12:00:18 +0100

    Add basic acoustic path tracing routine. The integrator simply doesn't crash. No meaningful data is recorded.
    Signed-off-by: leonardeyer <le0nard@me.com>

--- src/integrators/acousticpath.cpp
+++ src/integrators/acousticpath.cpp
@@ -1,40 +1,80 @@
-#include <random>
 #include <enoki/stl.h>
-#include <mitsuba/core/ray.h>
 #include <mitsuba/core/properties.h>
+#include <mitsuba/core/ray.h>
 #include <mitsuba/render/bsdf.h>
 #include <mitsuba/render/emitter.h>
 #include <mitsuba/render/integrator.h>
 #include <mitsuba/render/records.h>
+#include <random>
 
 NAMESPACE_BEGIN(mitsuba)
 
 template <typename Float, typename Spectrum>
 class AcousticPathIntegrator : public TimeDependentIntegrator<Float, Spectrum> {
 public:
-    MTS_IMPORT_BASE(TimeDependentIntegrator, m_max_time)
-    MTS_IMPORT_TYPES(Scene, Sampler, Medium, Emitter, EmitterPtr, BSDF, BSDFPtr)
+    MTS_IMPORT_BASE(TimeDependentIntegrator, m_stop, m_max_depth, m_rr_depth,
+                    m_max_time, m_time_steps, m_wavelength_bins)
+    MTS_IMPORT_TYPES(Scene, Sensor, Sampler, Medium, Emitter, EmitterPtr, BSDF,
+                     BSDFPtr)
 
-    AcousticPathIntegrator(const Properties &props) : Base(props) { }
+    AcousticPathIntegrator(const Properties &props) : Base(props) {}
 
-    std::pair<Spectrum, Mask> sample(const Scene *scene,
-                                     Sampler *sampler,
+    std::pair<Spectrum, Mask> sample(const Scene *scene, Sampler *sampler,
                                      const RayDifferential3f &ray_,
                                      const Medium * /* medium */,
                                      Float * /* aovs */,
                                      Mask active) const override {
         MTS_MASKED_FUNCTION(ProfilerPhase::SamplingIntegratorSample, active);
 
-        Throw("\"sample\" not implemented");
+        RayDifferential3f ray = ray_;
+
+        // MIS weight for intersected emitters (set by prev. iteration)
+        Float emission_weight(1.f);
+
+        Spectrum throughput(1.f), result(0.f);
+
+        // ---------------------- First intersection ----------------------
+
+        SurfaceInteraction3f si = scene->ray_intersect(ray, active);
+        Mask valid_ray          = si.is_valid();
+        EmitterPtr emitter      = si.emitter(scene);
+
+        for (int depth = 1;; ++depth) {
+
+            // ---------------- Intersection with emitters ----------------
+
+            if (any_or<true>(neq(emitter, nullptr)))
+                result[active] +=
+                    emission_weight * throughput * emitter->eval(si, active);
+
+            active &= si.is_valid();
+
+            // Stop if we've exceeded the number of requested bounces, or
+            // if there are no more active lanes. Only do this latter check
+            // in GPU mode when the number of requested bounces is infinite
+            // since it causes a costly synchronization.
+            if ((uint32_t) depth >= (uint32_t) m_max_depth ||
+                ((!is_cuda_array_v<Float> || m_max_depth < 0) && none(active)))
+                break;
+        }
+
+        return { result, valid_ray };
     }
 
     //! @}
     // =============================================================
 
     std::string to_string() const override {
-        return tfm::format("AcousticPathIntegrator[\n"
-                           "  max_time = %i,\n"
-                           "]", m_max_time);
+        std::ostringstream oss;
+        oss << "AcousticPathIntegrator[" << std::endl
+            << "  stop = " << m_stop << "," << std::endl
+            << "  max_depth = " << m_max_depth << "," << std::endl
+            << "  rr_depth = " << m_rr_depth << "," << std::endl
+            << "  max_time = " << m_max_time << "," << std::endl
+            << "  time_steps = " << m_time_steps << "," << std::endl
+            << "  wavelength_bins = " << m_wavelength_bins;
+        oss << std::endl << "]";
+        return oss.str();
     }
 
     MTS_DECLARE_CLASS()
---
+++ src/spectra/tests/test_acoustic.py
@@ -0,0 +1,51 @@
+# Superficial testing. Basically a wrapper around irregular
+
+import mitsuba
+import pytest
+import enoki as ek
+import numpy as np
+
+MTS_SOUND_SPEED = 343
+
+
+def make_freq():
+    return mitsuba.core.xml.load_string('''
+        <spectrum version='2.0.0' type='acoustic'>
+            <float name="freq_min" value="20"/>
+            <float name="freq_max" value="20000"/>
+            <string name="frequencies" value="20, 40, 16000"/>
+            <string name="values" value="1, 2, .5"/>
+        </spectrum>''').expand()[0]
+
+
+def make_acoustic_step_width(step_width):
+    return mitsuba.core.xml.load_string(f'''
+        <spectrum version='2.0.0' type='acoustic'>
+            <float name="lambda_min" value="0.017"/>
+            <float name="lambda_max" value="17"/>
+            <integer name="octave_step_width" value="{step_width}"/>
+        </spectrum>''').expand()[0]
+
+
+def test01_eval_frequency(variant_scalar_spectral):
+    from mitsuba.render import SurfaceInteraction3f
+    obj = make_freq()
+
+    freqs = [20000, 16000, 40, 20]
+    values = [0, 0.5, 2, 1]
+    si = SurfaceInteraction3f()
+
+    for i in range(len(freqs)):
+        si.wavelengths = MTS_SOUND_SPEED / freqs[i]
+        assert np.allclose(obj.eval(si), values[i])
+
+
+def test02_sample_fractional_oct(variant_scalar_spectral):
+    from mitsuba.render import SurfaceInteraction3f
+
+    obj = make_acoustic_step_width(1)
+
+    si = SurfaceInteraction3f()
+
+    assert ek.allclose(obj.sample_spectrum(si, 0)[0], 0.01532125)
+    assert ek.allclose(obj.sample_spectrum(si, 1)[0], 15.32124721)
