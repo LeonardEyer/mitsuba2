84cdcf88c43a359274766cacaa8cd7f055128e8b
LeonardEyer <le0nard@me.com>
2021-06-01 12:06:34 +0200

    Add first own diffuse scattering. (Not relying on blendbsdf). Test for different scattering/absorption values.

--- src/bsdfs/acousticbsdf.cpp
+++ src/bsdfs/acousticbsdf.cpp
@@ -28,67 +28,92 @@ public :
                                              Mask active) const override {
         MTS_MASKED_FUNCTION(ProfilerPhase::BSDFSample, active);
 
-        Float scatter = clamp(m_scatter->eval_1(si, active), 0.f, 1.f);
-
-//        Mask m0 = active && sample1 >  scatter,
-//             m1 = active && sample1 <= scatter;
-
         Float cos_theta_i = Frame3f::cos_theta(si.wi);
         active &= cos_theta_i > 0.f;
+
+        Float scatter = clamp(m_scatter->eval_1(si, active), 0.f, 1.f);
+
+        Mask m0 = active && sample1 >  scatter,
+             m1 = active && sample1 <= scatter;
 
         auto bs = zero<BSDFSample3f>();
         Spectrum result(0.f);
 
         UnpolarizedSpectrum reflectance = 1.f - m_absorpt->eval(si, active);
 
-        bs.sampled_component = 0;
-        bs.sampled_type = +BSDFFlags::DeltaReflection;
-        bs.wo  = reflect(si.wi);
-        bs.eta = 1.f;
-        bs.pdf = 1.f;
-        result = reflectance;
+        if (any_or<true>(m0)) {
+            // Specular component
+            auto bs_specular = zero<BSDFSample3f>();
 
-//        if (any_or<true>(m0)) {
-//            // Specular component
-//            auto bs_specular = zero<BSDFSample3f>();
-//
-//            bs_specular.sampled_component = 0;
-//            bs_specular.sampled_type = +BSDFFlags::DeltaReflection;
-//            bs_specular.wo  = reflect(si.wi);
-//            bs_specular.eta = 1.f;
-//            bs_specular.pdf = 1.f;
-//
-//            masked(bs, m0) = bs_specular;
-//            masked(result, m0) = reflectance * (1.f - scatter);
-//        }
-//
-//        if (any_or<true>(m1)) {
-//            // Diffuse component
-//            auto bs_diffuse = zero<BSDFSample3f>();
-//
-//            bs_diffuse.wo = warp::square_to_cosine_hemisphere(sample2);
-//            bs_diffuse.pdf = warp::square_to_cosine_hemisphere_pdf(bs.wo);
-//            bs_diffuse.eta = 1.f;
-//            bs_diffuse.sampled_type = +BSDFFlags::DiffuseReflection;
-//            bs_diffuse.sampled_component = 0;
-//
-//            masked(bs, m1) = bs_diffuse;
-//            masked(result, m1) = reflectance * scatter;
-//
-//        }
+            bs_specular.sampled_component = 0;
+            bs_specular.sampled_type = +BSDFFlags::DeltaReflection;
+            bs_specular.wo  = reflect(si.wi);
+            bs_specular.eta = 1.f;
+            bs_specular.pdf = 1.f;
+
+            masked(bs, m0) = bs_specular;
+            masked(result, m0) = reflectance * (1.f - scatter);
+        }
+
+        if (any_or<true>(m1)) {
+            // Diffuse component
+            auto bs_diffuse = zero<BSDFSample3f>();
+
+            bs_diffuse.wo = warp::square_to_cosine_hemisphere(sample2);
+            bs_diffuse.pdf = warp::square_to_cosine_hemisphere_pdf(bs.wo);
+            bs_diffuse.eta = 1.f;
+            bs_diffuse.sampled_type = +BSDFFlags::DiffuseReflection;
+            bs_diffuse.sampled_component = 0;
+
+            masked(bs, m1) = bs_diffuse;
+            masked(result, m1) = reflectance * scatter;
+
+        }
 
         return { bs, result & active };
     }
 
     Spectrum eval(const BSDFContext &ctx, const SurfaceInteraction3f &si,
                   const Vector3f &wo, Mask active) const override {
+        MTS_MASKED_FUNCTION(ProfilerPhase::BSDFEvaluate, active);
 
-        return 0.f;
+        Float cos_theta_i = Frame3f::cos_theta(si.wi),
+              cos_theta_o = Frame3f::cos_theta(wo);
+
+        active &= cos_theta_i > 0.f && cos_theta_o > 0.f;
+
+        Float scatter = clamp(m_scatter->eval_1(si, active), 0.f, 1.f);
+
+        UnpolarizedSpectrum reflectance = 1.f - m_absorpt->eval(si, active);
+
+        UnpolarizedSpectrum value_specular = reflectance * select(reflect(si.wi) == wo, 1.f, 0.f);
+        UnpolarizedSpectrum value_diffuse = reflectance * math::InvPi<Float> * cos_theta_o;
+
+        return select(active,
+                      (value_specular * (1.f - scatter)) +
+                         (value_diffuse * scatter),
+                      0.f);
     }
 
     Float pdf(const BSDFContext &ctx, const SurfaceInteraction3f &si,
               const Vector3f &wo, Mask active) const override {
-        return 0.f;
+
+        MTS_MASKED_FUNCTION(ProfilerPhase::BSDFEvaluate, active);
+
+        Float cos_theta_i = Frame3f::cos_theta(si.wi),
+              cos_theta_o = Frame3f::cos_theta(wo);
+
+        active &= cos_theta_i > 0.f && cos_theta_o > 0.f;
+
+        Float scatter = clamp(m_scatter->eval_1(si, active), 0.f, 1.f);
+
+        Float pdf_specular = (select(reflect(si.wi) == wo, 1.f, 0.f));
+        Float pdf_diffuse = warp::square_to_cosine_hemisphere_pdf(wo);
+
+        return select(active,
+                      (pdf_specular * (1.f - scatter)) +
+                         (pdf_diffuse * scatter),
+                      0.f);
     }
 
     void traverse(TraversalCallback *callback) override {
